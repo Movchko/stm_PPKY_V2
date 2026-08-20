320ed7432d5b8f10ccbfcddab57b184ae6fddc8e
Movchko <movchko.evgeny@gmail.com>
2026-08-20 17:35:18 +0300

    исправление бага как в ппкув1. слияние хвоста.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1060,7 +1060,7 @@ static void MX_GPIO_Init(void)
 uint8_t SetUpdateWord(uint32_t num, uint32_t word)
 { return 0;}
 
-#define APP_VERSION_U32 2u
+#define APP_VERSION_U32 3u
 
 const char *GetAppVersion(void)
 {
